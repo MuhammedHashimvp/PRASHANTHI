9ddb8a90db47f98c4038eab4c10ca21bd7e8b454
ArshaqAk <arshaqakard@gmail.com>
2025-04-05 22:13:12 +0530

    typewriter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,46 @@
 import React from 'react'
 import '../styles/home.css'
+import { Typewriter } from 'react-simple-typewriter'
 
 const Home = () => {
   return (
-    <div className='container-fluid home-container'>
-        <div className="row p-5">
-            {/* left */}
-            <div className="col-lg-6 col-sm-12  left-div">
-                <h1 className='title'>Expert Medical Care <br /> Starts with the</h1>
-                <h1 className='title'><span>Right Doctor</span></h1>
-                <p>Connect with trusted doctors online for expert advice. Get quick, reliable <br /> consultations from certified professionals—anytime, anywhere</p>
-                <button className='appointment-btn btn shadow w-25'>Appointment Now</button>
-            </div>
-            {/* right */}
-            <div className="col-lg-6 col-sm-12  right-div">
+    <div className='container-fluid p-4 home-container'>
+      <div className="row">
+        {/* left */}
+        <div className="col-lg-6 col-sm-12 left-div">
+            <h1 className='title'>
+            <span style={{ color: '#003152' }}>We Provide&nbsp;</span>
+            <span style={{ color: 'green', fontWeight: 'bold' }}>
+          <Typewriter 
+                  words={[
+                  'Expert Medical Care',
+                  'Trusted Doctors',
+                  '24/7 Online Support',
+                  'Affordable Health Services'
+                  ]}
+                  loop={0}
+                  cursor
+                  cursorStyle='|'
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1400}
+                />
+              </span>
+            </h1>
 
-            </div>
+
+          <p>
+            Connect with trusted doctors online for expert advice. Get quick, reliable <br />
+            consultations from certified professionals—anytime, anywhere.
+          </p>
+          <button className='appointment-btn btn shadow w-50'>Appointment Now</button>
         </div>
+
+        {/* right */}
+        <div className="col-lg-6 col-sm-12 right-div">
+
+        </div>
+      </div>
     </div>
   )
 }

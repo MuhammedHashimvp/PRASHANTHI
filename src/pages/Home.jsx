80abfa16cbabe7e6705189e1db0c680d7229ee33
MuhammedHashimvp <mhdhashimvp@gmail.com>
2025-04-05 15:10:24 +0530

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ import '../styles/home.css'
 const Home = () => {
   return (
     <div className='container-fluid home-container'>
-        <div className="row p-4">
+        <div className="row p-5">
             {/* left */}
             <div className="col-lg-6 col-sm-12  left-div">
                 <h1 className='title'>Expert Medical Care <br /> Starts with the</h1>
                 <h1 className='title'><span>Right Doctor</span></h1>
                 <p>Connect with trusted doctors online for expert advice. Get quick, reliable <br /> consultations from certified professionals—anytime, anywhere</p>
-                <button>Appoint Now</button>
+                <button className='appointment-btn btn shadow w-25'>Appointment Now</button>
             </div>
             {/* right */}
             <div className="col-lg-6 col-sm-12  right-div">
